9db9cc49eca5ad5e186be86a3474532328ffc719
jessicamlee <jessicamlee25@gmail.com>
2022-11-23 23:54:26 -0800

    Fix project setup errors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-export default function App() {
+function App() {
   return (
-    <div className="App">
+    <div>
       <h1 className="text-3xl text-white text-center font-bold underline bg-blue-800 py-8">
         Hello World!
       </h1>
     </div>
   );
 }
+
+export default App;
