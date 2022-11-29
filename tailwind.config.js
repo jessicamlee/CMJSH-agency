d2f2083c73098e1609977878864a8429fa9a4a55
jessicamlee <jessicamlee25@gmail.com>
2022-11-28 21:25:41 -0800

    :wrench: Install Flowbite & Flowbite-React

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+  ],
   darkMode: 'class',
   theme: {
     screens: {
@@ -45,5 +48,7 @@ module.exports = {
       }
     }
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
