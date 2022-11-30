558db01b0093f53cf0f2d3bd5c2ca7c55bb0b00a
MacolmTann <malcolmtan20@gmail.com>
2022-11-30 02:26:28 -0800

    Updated structure of header
    - removed the Bg

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,17 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header className='text-white bg-darkBlue p-6 sticky top-0 flex justify-between items-center z-1 lg:px-10'>
+    <header className='sticky text-white p-6 top-0 z-1 -mt-24 lg:px-10'>
+      <nav className='flex justify-between items-center'>
         <h2 className='font-Overpass font-bold text-xl mt-1 lg:text-2xl xl:text-3xl'>CMJSH</h2>
         <img className='h-10 w-10 lg:hidden' src='assets/images/burger.svg' alt='menu'></img>
-        <nav className='hidden lg:block'>
-          <ul className='flex gap-x-10 font-light xl:text-base'>
-            <li><a href='#' title='Home'>Home</a></li>
-            <li><a href='#' title='Home'>About</a></li>
-            <li><a href='#' title='Home'>Work</a></li>
-            <li><a href='#' title='Home'>Contact</a></li>
-          </ul>
-        </nav>
+        <ul className='gap-x-10 font-light hidden lg:flex xl:text-base'>
+          <li><a href='#' title='Home'>Home</a></li>
+          <li><a href='#' title='Home'>About</a></li>
+          <li><a href='#' title='Home'>Work</a></li>
+          <li><a href='#' title='Home'>Contact</a></li>
+        </ul>
+      </nav>
     </header>
   )
 }
