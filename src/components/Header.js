a8be38c250ff70fa54c4388d96bdccf24ba55814
MacolmTann <malcolmtan20@gmail.com>
2022-11-29 23:52:10 -0800

    Added burger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className='text-white bg-darkBlue py-7 px-6 sticky top-0 flex justify-between items-center z-1'>
         <div className='font-bold text-xl'>CMJSH</div>
-        <div>Menu</div>
+        <img className='h-9 w-9' src='assets/images/burger.svg' alt='menu'></img>
     </header>
   )
 }
