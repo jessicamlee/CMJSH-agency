8fbbfea2444c337b8cc6678680483d5d99abbc53
MacolmTann <malcolmtan20@gmail.com>
2022-11-30 02:38:05 -0800

    Adjust H1 positioning

--- src/layout/Hero.js
+++ src/layout/Hero.js
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <div className='bg-darkBlue text-white h-[36rem] flex justify-center items-center flex-col font-Overpass relative'>
         <video className='hidden md:block object-cover h-full w-full brightness-50' src='/assets/videos/videoBG.mp4' autoPlay muted loop />
-        <h1 className='absolute font-bold text-4xl md:text-5xl lg:text-7xl leading-8 mb-20'>Design<span className='block'>Discover</span>Develop</h1>
+        <h1 className='absolute font-bold text-4xl leading-8 mb-26 md:text-5xl lg:text-7xl '>Design<span className='block'>Discover</span>Develop</h1>
     </div>
   )
 }
