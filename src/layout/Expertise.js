ec607d3ce71dc6e6cdff982e09f3a5de6a97df35
samantha-t28 <samanthatan228@gmail.com>
2022-11-30 17:16:37 -0800

    💄 🍱 Expertise section & added assets
    - added SVG icons
    - team profile photo
    - aligned expertise section with 'who are we'

--- src/layout/Expertise.js
+++ src/layout/Expertise.js
@@ -3,7 +3,7 @@ import { dataExpertise } from "../constants";
 
 export default function Expertise() {
 	return (
-		<div className="grid grid-cols-1 gap-16 section-spacing relative">
+		<div className="grid grid-cols-1 gap-16 w-md pl-8 md:pl-14">
 			<h2 className="text-4xl text-darkBlue font-bold border-b-4 w-fit border-lightBlue">
 				Our Expertise
 			</h2>
