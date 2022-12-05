a5558c7c22ce3eeb2902b8a3918b8bc233410a0b
MacolmTann <malcolmtan20@gmail.com>
2022-12-05 03:01:33 -0800

    Update ProfilePage.js

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -12,27 +12,25 @@ export default function Profiles() {
     return (
         <>
             <HeaderPlain />
-            <Fade cascade>
-                <main className="mt-48 mb-8 min-h-justBody">
-                    <div className="flex justify-center items-center flex-col relative">
-                        <h1 className="text-4xl md:text-5xl text-darkBlue font-bold border-b-4 w-fit border-lightBlue md:mx-auto dark:text-white">
-                            Meet the Team
-                        </h1>
-                        <div className="h-232 457wide:h-240 sm:h-292 md:h-128 px-8 container max-w-screen-xs md:max-w-screen-md mx-auto mt-8 md:mt-9 mb-10 md:mb-9">
-                            <Carousel 
-                                slide={false}
-                                indicators={false}
-                                leftControl="<"
-                                rightControl=">"
-                            >
-                                {dataTeamMembers.map((item) => {
-                                    return ProfileCard(item);
-                                })}
-                            </Carousel>
-                        </div>
+            <main className="mt-48 mb-8 min-h-justBody">
+                <div className="flex justify-center items-center flex-col relative">
+                    <h1 className="text-4xl md:text-5xl text-darkBlue font-bold border-b-4 w-fit border-lightBlue md:mx-auto dark:text-white">
+                        Meet the Team
+                    </h1>
+                    <div className="h-232 457wide:h-240 sm:h-292 md:h-128 px-8 container max-w-screen-xs md:max-w-screen-md mx-auto mt-8 md:mt-9 mb-10 md:mb-9">
+                        <Carousel 
+                            slide={false}
+                            indicators={false}
+                            leftControl="<"
+                            rightControl=">"
+                        >
+                            {dataTeamMembers.map((item) => {
+                                return ProfileCard(item);
+                            })}
+                        </Carousel>
                     </div>
-                </main>
-            </Fade>
+                </div>
+            </main>
             <Footer />
         </>
     )
