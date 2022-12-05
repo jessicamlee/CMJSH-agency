81f4c94fab825f423c647fd59484c1142763690a
MacolmTann <malcolmtan20@gmail.com>
2022-12-05 03:11:45 -0800

    Attempt to fix errors

--- src/layout/Contact.js
+++ src/layout/Contact.js
@@ -4,23 +4,23 @@ import Fade from 'react-reveal/Fade';
 export default function Contact() {
 
   return (
-    <Fade>
-      <div className='grid grid-cols-1 px-6 mb-8 text-sm mt-40 md:mb-12 md:mt-52 lg:text-base lg:px-28 xl:px-44 relative xl:max-w-screen-xl mx-auto'>
+    <div className='grid grid-cols-1 px-6 mb-8 text-sm mt-40 md:mb-12 md:mt-52 lg:text-base lg:px-28 xl:px-44 relative xl:max-w-screen-xl mx-auto'>
+        <Fade>
           <p className='font-light mb-1 text-sm'>We're always glad to help</p>
           <h2 className="text-4xl text-darkBlue font-bold border-b-4 w-fit border-lightBlue mb-20 lg:mb-28">Contact Us</h2>
-          <Fade cascade bottom>
-            <form className='flex flex-col gap-10 mb-12 md:flex-row md:flex-wrap md:px-14 md:gap-x-28 lg:px-0'>
-                <input className='border-b pb-3 grow border-0 border-inherit' type='text' placeholder='Your name' required></input>
-                <input className='border-b pb-3 grow border-0 border-inherit' type='email' placeholder='Your email' required></input>
-                <textarea className='border-b w-full mt-7 border-0 border-inherit' type='text' placeholder='Your Message' required></textarea>
-                <button className='bg-darkBlue text-white text-sm rounded-full w-fit flex gap-3 items-center py-1 pl-3 pr-1 mt-6 mx-auto' type='submit'>
-                    <span className='pl-3'>Send</span>
-                    <img className='bg-white rounded-full h-7 w-7' src='/assets/images/sendarrow.svg' alt='arrow'></img>
-                </button>
-            </form>
-          </Fade>
-          <div id='validate' className='absolute text-green-600 mx-auto bottom-2 left-0 right-0 w-fit hidden'>Message successfully sent!</div>
-      </div>
-    </Fade>
+        </Fade>
+        <Fade cascade bottom>
+          <form className='flex flex-col gap-10 mb-12 md:flex-row md:flex-wrap md:px-14 md:gap-x-28 lg:px-0'>
+              <input className='border-b pb-3 grow border-0 border-inherit' type='text' placeholder='Your name' required></input>
+              <input className='border-b pb-3 grow border-0 border-inherit' type='email' placeholder='Your email' required></input>
+              <textarea className='border-b w-full mt-7 border-0 border-inherit' type='text' placeholder='Your Message' required></textarea>
+              <button className='bg-darkBlue text-white text-sm rounded-full w-fit flex gap-3 items-center py-1 pl-3 pr-1 mt-6 mx-auto' type='submit'>
+                  <span className='pl-3'>Send</span>
+                  <img className='bg-white rounded-full h-7 w-7' src='/assets/images/sendarrow.svg' alt='arrow'></img>
+              </button>
+          </form>
+        </Fade>
+        <div id='validate' className='absolute text-green-600 mx-auto bottom-2 left-0 right-0 w-fit hidden'>Message successfully sent!</div>
+    </div>
   )
 }
