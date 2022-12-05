522781cd45ba49d7e2f9e08f8f1e98c8033236ec
MacolmTann <malcolmtan20@gmail.com>
2022-12-05 03:05:18 -0800

    Update TeamMemberCard.js

--- src/components/TeamMemberCard.js
+++ src/components/TeamMemberCard.js
@@ -17,14 +17,12 @@ export default function TeamMemberCard({
 	}
 
 	return (
-		<Fade cascade>
-			<li key={id} className="w-47pct md:w-28pct xl:w-22pct xl:max-w-22pct">
-				<img className="rounded w-full" src={imagePath} alt={imageName} />
-				<h3 className="text-darkBlue text-base lg:text-2xl font-bold leading-5 mt-3 dark:text-white">{name}</h3>
-				<p className="text-roleDark text-xs md:text-sm lg:text-base font-light leading-4 mt-2 lg:mt-0 lg:font-regular dark:text-roleLight">{role}</p>
-				<p className="text-darkBlue text-xs font-light mt-2 md:text-sm xl:font-regular dark:text-white">{paragraph}</p>
-				<button className='btn-full' title="Go to Profile page" onClick={navigateToProfile}>View Work</button>
-			</li>
-		</Fade>
+		<li key={id} className="w-47pct md:w-28pct xl:w-22pct xl:max-w-22pct">
+			<img className="rounded w-full" src={imagePath} alt={imageName} />
+			<h3 className="text-darkBlue text-base lg:text-2xl font-bold leading-5 mt-3 dark:text-white">{name}</h3>
+			<p className="text-roleDark text-xs md:text-sm lg:text-base font-light leading-4 mt-2 lg:mt-0 lg:font-regular dark:text-roleLight">{role}</p>
+			<p className="text-darkBlue text-xs font-light mt-2 md:text-sm xl:font-regular dark:text-white">{paragraph}</p>
+			<button className='btn-full' title="Go to Profile page" onClick={navigateToProfile}>View Work</button>
+		</li>
 	);
 }
