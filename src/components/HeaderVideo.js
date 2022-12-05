6bad581697fd32304028567c522ff619702b0011
samantha-t28 <samanthatan228@gmail.com>
2022-12-05 04:31:59 -0800

    💄 Added hover states to the header

--- src/components/HeaderVideo.js
+++ src/components/HeaderVideo.js
@@ -1,68 +1,115 @@
 import { useState } from "react";
 
 export default function HeaderVideo() {
+	const [navbar, setNavbar] = useState(false);
 
-  const [navbar, setNavbar] = useState(false);
+	const showNavBg = () => {
+		if (window.scrollY >= 576) {
+			setNavbar(true);
+		} else {
+			setNavbar(false);
+		}
+	};
 
-  const showNavBg = () => {
-    if (window.scrollY >= 576) {
-      setNavbar(true);
-    } else {
-      setNavbar(false);
-    }
-  };
+	window.addEventListener("scroll", showNavBg);
 
-  window.addEventListener('scroll', showNavBg);
+	const [isNavOpen, setIsNavOpen] = useState(false);
 
-  const [isNavOpen, setIsNavOpen] = useState(false);
+	return (
+		<header className="relative text-white p-6 top-0 z-1 -mt-24">
+			<nav className={navbar ? "navbar active" : "navbar"}>
+				<h2 className="font-Overpass font-bold text-xl mt-1 lg:text-2xl xl:text-3xl">
+					<a href="/" title="Go to Home page">
+						CMJSH
+					</a>
+				</h2>
 
-  return (
-    <header className='relative text-white p-6 top-0 z-1 -mt-24'>
-      <nav className={navbar ? 'navbar active' : 'navbar'}>
-        <h2 className='font-Overpass font-bold text-xl mt-1 lg:text-2xl xl:text-3xl'><a href="/" title="Go to Home page">CMJSH</a></h2>
+				{/* Mobile Menu */}
+				<div className="flex lg:hidden">
+					<img
+						className="h-10 w-10"
+						onClick={() => setIsNavOpen((prev) => !prev)}
+						src="assets/images/burger.svg"
+						alt="menu"
+					></img>
+					<div className={isNavOpen ? "showNav" : "hideNav"}>
+						<div
+							className="absolute top-0 right-0 p-8"
+							onClick={() => setIsNavOpen(false)}
+						>
+							<svg
+								className="h-8 w-8 text-white fill-none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+								strokeWidth="2"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+							>
+								<line x1="18" y1="6" x2="6" y2="18" />
+								<line x1="6" y1="6" x2="18" y2="18" />
+							</svg>
+						</div>
+						<ul className="nav-items-mobile">
+							<li>
+								<a href="/" title="Home">
+									Home
+								</a>
+							</li>
+							<li>
+								<a href="/" title="About Us">
+									About
+								</a>
+							</li>
+							<li>
+								<a href="/" title="Our Work">
+									Work
+								</a>
+							</li>
+							<li>
+								<a href="/" title="Contact Us">
+									Contact
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
 
-        {/* Mobile Menu */}
-        <div className="flex lg:hidden">
-          <img 
-            className='h-10 w-10' 
-            onClick={() => setIsNavOpen((prev) => !prev)}
-            src='assets/images/burger.svg' 
-            alt='menu'
-          ></img>
-          <div className={isNavOpen ? "showNav" : "hideNav"}>
-            <div 
-              className="absolute top-0 right-0 p-8"
-              onClick={() => setIsNavOpen(false)}
-            >
-              <svg
-                  className="h-8 w-8 text-white fill-none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <line x1="18" y1="6" x2="6" y2="18" />
-                  <line x1="6" y1="6" x2="18" y2="18" />
-              </svg>
-            </div>
-            <ul className='nav-items-mobile'>
-              <li><a href='/' title='Home'>Home</a></li>
-              <li><a href='/' title='About Us'>About</a></li>
-              <li><a href='/' title='Our Work'>Work</a></li>
-              <li><a href='/' title='Contact Us'>Contact</a></li>
-            </ul>
-          </div>
-        </div>
-
-        {/* Desktop Menu */}
-        <ul className='nav-items-desktop'>
-          <li><a href='/' title='Home'>Home</a></li>
-          <li><a href='/' title='About Us'>About</a></li>
-          <li><a href='/' title='Our Work'>Work</a></li>
-          <li><a href='/' title='Contact Us'>Contact</a></li>
-        </ul>
-      </nav>
-    </header>
-  )
+				{/* Desktop Menu */}
+				<ul className="nav-items-desktop">
+					<li>
+						<a className="hover:text-sky-300" href="/" title="Home">
+							Home
+						</a>
+					</li>
+					<li>
+						<a
+							className="hover:text-sky-300"
+							href="/"
+							title="About Us"
+						>
+							About
+						</a>
+					</li>
+					<li>
+						<a
+							className="hover:text-sky-300"
+							href="/"
+							title="Our Work"
+						>
+							Work
+						</a>
+					</li>
+					<li>
+						<a
+							className="hover:text-sky-300"
+							href="/"
+							title="Contact Us"
+						>
+							Contact
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 }
