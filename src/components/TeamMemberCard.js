830d42fdc0b5918edfd4b199f373c81873b230d8
samantha-t28 <samanthatan228@gmail.com>
2022-12-06 19:05:31 -0800

    💄styled hover state on SVG and buttons
    - who are we, contact, footer SVG, team member card
    - changed opacity from 0.8 to 2 (SVG icons in footer)

--- src/components/TeamMemberCard.js
+++ src/components/TeamMemberCard.js
@@ -1,29 +1,47 @@
 import { useNavigate } from "react-router-dom";
-import Fade from 'react-reveal/Fade';
+import Fade from "react-reveal/Fade";
 
 export default function TeamMemberCard({
 	id,
 	name,
-    role,
+	role,
 	paragraph,
 	imagePath,
 	imageName,
 }) {
-
 	const navigate = useNavigate();
 
-    function navigateToProfile() {
-		navigate('/ProfilePage');
+	function navigateToProfile() {
+		navigate("/ProfilePage");
 	}
 
 	return (
 		<Fade cascade>
-			<li key={id} className="w-47pct md:w-28pct xl:w-22pct xl:max-w-22pct">
-				<img className="rounded w-full" src={imagePath} alt={imageName} />
-				<h3 className="text-darkBlue text-base lg:text-2xl font-bold leading-5 mt-3 dark:text-white">{name}</h3>
-				<p className="text-roleDark text-xs md:text-sm lg:text-base font-light leading-4 mt-2 lg:mt-0 lg:font-regular dark:text-roleLight">{role}</p>
-				<p className="text-darkBlue text-xs font-light mt-2 md:text-sm xl:font-regular dark:text-white">{paragraph}</p>
-				<button className='btn-full' title="Go to Profile page" onClick={navigateToProfile}>View Work</button>
+			<li
+				key={id}
+				className="w-47pct md:w-28pct xl:w-22pct xl:max-w-22pct"
+			>
+				<img
+					className="rounded w-full"
+					src={imagePath}
+					alt={imageName}
+				/>
+				<h3 className="text-darkBlue text-base lg:text-2xl font-bold leading-5 mt-3 dark:text-white">
+					{name}
+				</h3>
+				<p className="text-roleDark text-xs md:text-sm lg:text-base font-light leading-4 mt-2 lg:mt-0 lg:font-regular dark:text-roleLight">
+					{role}
+				</p>
+				<p className="text-darkBlue text-xs font-light mt-2 md:text-sm xl:font-regular dark:text-white">
+					{paragraph}
+				</p>
+				<button
+					className="btn-full hover:bg-sky-300"
+					title="Go to Profile page"
+					onClick={navigateToProfile}
+				>
+					View Work
+				</button>
 			</li>
 		</Fade>
 	);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,94 +1,100 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
-  ],
-  darkMode: 'class',
-  theme: {
-    screens: {
-      '457wide': '457px',
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1440px'
-    },
+	content: [
+		"./src/**/*.{js,jsx,ts,tsx}",
+		"node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+	],
+	darkMode: "class",
+	theme: {
+		screens: {
+			"457wide": "457px",
+			sm: "640px",
+			md: "768px",
+			lg: "1024px",
+			xl: "1440px",
+		},
 
-    extend: {
+		extend: {
+			backgroundImage: {
+				"light-768home":
+					"url('assets/images/bg/homepage/768HomePageGradient.png')",
+				"light-1024home":
+					"url('assets/images/bg/homepage/1024HomePageGradient.png')",
+				"light-1440home":
+					"url('assets/images/bg/homepage/1440HomePageGradient.png')",
+				"light-768profile":
+					"url('assets/images/bg/profilepage/768ProfileLight.png')",
+				"light-1024profile":
+					"url('assets/images/bg/profilepage/1024ProfileLight.png')",
+				"light-1440profile":
+					"url('assets/images/bg/profilepage/1440ProfileLight.png')",
+				"dark-768profile":
+					"url('assets/images/bg/profilepage/768ProfileDark.png')",
+				"dark-1024profile":
+					"url('assets/images/bg/profilepage/1024ProfileDark.png')",
+				"dark-1440profile":
+					"url('assets/images/bg/profilepage/1440ProfileDark.png')",
+			},
 
-      backgroundImage: {
-        'light-768home': "url('assets/images/bg/homepage/768HomePageGradient.png')",
-        'light-1024home': "url('assets/images/bg/homepage/1024HomePageGradient.png')",
-        'light-1440home': "url('assets/images/bg/homepage/1440HomePageGradient.png')",
-        'light-768profile': "url('assets/images/bg/profilepage/768ProfileLight.png')",
-        'light-1024profile': "url('assets/images/bg/profilepage/1024ProfileLight.png')",
-        'light-1440profile': "url('assets/images/bg/profilepage/1440ProfileLight.png')",
-        'dark-768profile': "url('assets/images/bg/profilepage/768ProfileDark.png')",
-        'dark-1024profile': "url('assets/images/bg/profilepage/1024ProfileDark.png')",
-        'dark-1440profile': "url('assets/images/bg/profilepage/1440ProfileDark.png')",
-      },
+			colors: {
+				darkBlue: "rgba(7, 8, 46, 1)",
+				lightBlue: "rgba(79, 177, 212, 1)",
+				vlightBlue: "rgba(162, 222, 237, 0.5)",
+				roleDark: "rgba(2, 4, 90, 1)",
+				roleLight: "rgba(209, 239, 247, 1)",
+				darkBg: "rgba(67,68,96,255)",
+			},
 
-      colors: {
-        darkBlue: 'rgba(7, 8, 46, 1)',
-        lightBlue: 'rgba(79, 177, 212, 1)',
-        vlightBlue: 'rgba(162, 222, 237, 0.5)',
-        roleDark: 'rgba(2, 4, 90, 1)',
-        roleLight: 'rgba(209, 239, 247, 1)',
-        darkBg: 'rgba(67,68,96,255)',
-      },
+			fontFamily: {
+				Overpass: "'Overpass', sans-serif",
+			},
 
-      fontFamily: {
-        Overpass: "'Overpass', sans-serif",
-      },
+			fontWeight: {
+				light: "300",
+				regular: "400",
+				medium: "500",
+				bold: "700",
+			},
 
-      fontWeight: {
-        light: '300',
-        regular: '400',
-        medium: '500',
-        bold: '700',
-      },
+			lineHeight: {
+				0: "0em",
+			},
 
-      lineHeight: {
-        '0': '0em',
-      },
+			margin: {
+				0.3: "0.3em",
+				0.35: "0.35em",
+			},
 
-      margin: {
-        '0.3':'0.3em',
-        '0.35': '0.35em',
-      },
+			width: {
+				"22pct": "22%",
+				"28pct": "28%",
+				"47pct": "47%",
+			},
 
-      width: {
-        '22pct': '22%',
-        '28pct': '28%',
-        '47pct': '47%',
-      },
-  
-      maxWidth: {
-        '22pct': '22%'
-      },
+			maxWidth: {
+				"22pct": "22%",
+			},
 
-      height: {
-        '128': '32rem',
-        '200': '50rem',
-        '232': '58rem',
-        '240': '66rem',
-        '292': '73rem',
-      },
+			height: {
+				128: "32rem",
+				200: "50rem",
+				232: "58rem",
+				240: "66rem",
+				292: "73rem",
+			},
 
-      minHeight: {
-        'justBody': 'calc(100vh - 350px)',
-      },
+			minHeight: {
+				justBody: "calc(100vh - 350px)",
+			},
 
-      spacing: {
-        '50-': '-50px'
-      },
-      
-      zIndex: {
-        '1': '1',
-      },
-    }
-  },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-}
+			spacing: {
+				"50-": "-50px",
+			},
+
+			zIndex: {
+				1: "1",
+			},
+		},
+	},
+	plugins: [require("flowbite/plugin")],
+};
