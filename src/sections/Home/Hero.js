ee6aa38b0d96f37fbf8d9a4f4ba9df1221569115
jessicamlee <jessicamlee25@gmail.com>
2022-12-30 21:49:37 -0800

    :lipstick: :fire: Add/Rename files and folders
    - Separate constants data into two files as not all data is needed when called upon for specific pages (Team Members and Profile cards)
    - Update profile pictures in src folder
    - Rename "HomePage" and "ProfilePage" files as "Home" and "OurTeam," respectively, to provide cleaner URL

---
+++ src/sections/Home/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Fade from 'react-reveal/Fade';
+import HeroVideo from '../../assets/videos/videoBg.mp4';
+
+export default function Hero() {
+  return (
+    <div className='bg-darkBlue text-white h-[36rem] flex justify-center items-center flex-col relative mb-52 lg:h-[44rem]'>
+        <video className='hidden md:block object-cover h-full w-full brightness-50' src={HeroVideo} autoPlay muted loop />
+        <Fade cascade bottom>
+          <h1 className='absolute font-bold text-4xl leading-8 mt-12 md:text-5xl lg:text-7xl'><span className='block'>Design</span><span className='block'>Discover</span><span className='block'>Develop</span></h1>
+        </Fade>
+    </div>
+  )
+}
